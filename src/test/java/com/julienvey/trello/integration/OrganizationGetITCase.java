cca9b5f9afe839cd02f02d5e30b9d3ab79002ac8
eferroja@gmail.com <4m4t3r4su>
2016-11-29 11:30:56 +0000

    Gitig

--- src/test/java/com/julienvey/trello/integration/OrganizationGetITCase.java
+++ src/test/java/com/julienvey/trello/integration/OrganizationGetITCase.java
@@ -1,8 +1,50 @@
 package com.julienvey.trello.integration;
 
+import com.julienvey.trello.Trello;
+import com.julienvey.trello.TrelloHttpClient;
+import com.julienvey.trello.domain.Organization;
+import com.julienvey.trello.impl.TrelloImpl;
+import com.julienvey.trello.impl.http.ApacheHttpClient;
+import com.julienvey.trello.impl.http.AsyncTrelloHttpClient;
+import com.julienvey.trello.impl.http.RestTemplateHttpClient;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+@RunWith(Parameterized.class)
 public class OrganizationGetITCase {
     private static final String TEST_APPLICATION_KEY = "db555c528ce160c33305d2ea51ae1197";
     public static final String ORG_ID = "518baaaa815af84031004375";
 
+    private Trello trello;
 
+    private TrelloHttpClient httpClient;
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{{new ApacheHttpClient()}, {new AsyncTrelloHttpClient()}, {new RestTemplateHttpClient()}});
+    }
+
+    public OrganizationGetITCase(TrelloHttpClient httpClient) {
+        this.httpClient = httpClient;
+    }
+
+    @Before
+    public void setUp() {
+        trello = new TrelloImpl(TEST_APPLICATION_KEY, "", httpClient);
+    }
+
+    @Test
+    public void testGetOrganization() {
+        Organization organization = trello.getOrganization(ORG_ID);
+
+        assertThat(organization).isNotNull();
+        assertThat(organization.getId()).isEqualTo(ORG_ID);
+    }
 }
